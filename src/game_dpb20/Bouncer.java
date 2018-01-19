f5af95472730e6a97deb55934813e13779b22ee5
Dorian Barber <dpb20@duke.edu>
2018-01-19 16:12:27 -0500

    Wall collision detection

--- src/game_dpb20/Bouncer.java
+++ src/game_dpb20/Bouncer.java
@@ -4,11 +4,11 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Bouncer extends Circle{
-	private static Color myColor = Color.AQUA;
+	private static Color myColor = Color.LINEN;
 	private static int myRadius = 8;
 	
-	private int xvel = 5;
-	private int yvel = 5;
+	private int xvel = -50;
+	private int yvel = -50;
 	
 	public Bouncer() {
 		super(myRadius, myColor);
@@ -22,13 +22,25 @@ public class Bouncer extends Circle{
 		yvel = -yvel;
 	}
 	
+	public int getXVel() {
+		return xvel;
+	}
+	
+	public int getYVel() {
+		return yvel;
+	}
+	
+	//checks if the ball has hit the y border (top of the screen)
+	//if so it returns false
 	public boolean checkYBounds() {
 		return (this.getCenterY()-myRadius) >= 0;
 	}
 	
+	//checks if the ball has hit either the right or left side of the screen
+	//if so returns false
 	public boolean checkXBounds(int border) {
-		return (this.getCenterX() - myRadius) >= 0 
-				&& (this.getCenterX() + myRadius) <= border; 
+		return ((this.getCenterX() - myRadius) >= 0 
+				&& (this.getCenterX() + myRadius) <= border); 
 	}
 	
 }
--- src/game_dpb20/Breakout.java
+++ src/game_dpb20/Breakout.java
@@ -1,18 +1,20 @@
 package game_dpb20;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 
 public class Breakout extends Application{
 	public static final int FRAMES_PER_SECOND = 60;
 	public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+	public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
 	public static final int SIZE = 500;
 	
 	public static final Paint BACKGROUND = Color.BURLYWOOD;
@@ -24,11 +26,22 @@ public class Breakout extends Application{
 	private Paddle myPaddle;
 	private Bouncer myBall;
 	
+	private boolean gameStart;
+	
 	
 	public void start(Stage stage) {
+		//sets up scene
 		myScene = setupGame(SIZE, SIZE, BACKGROUND);
 		stage.setScene(myScene);
 		stage.show();
+		
+		//sets up game loop
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+		Timeline animation = new Timeline(); 
+		animation.setCycleCount(Timeline.INDEFINITE);
+		animation.getKeyFrames().add(frame);
+		animation.play();
+		
 	}
 	
 	private Scene setupGame(int width, int height, Paint background) {
@@ -52,11 +65,21 @@ public class Breakout extends Application{
 	}
 	
 	private void step (double elapsedTime) {
-		
+		if(gameStart) {
+			if(!myBall.checkXBounds(SIZE)) {
+				myBall.bounceX();
+			}
+			myBall.setCenterX(myBall.getCenterX() + myBall.getXVel() * elapsedTime);
+			
+			if(!myBall.checkYBounds()) {
+				myBall.bounceY();
+			}
+			myBall.setCenterY(myBall.getCenterY() + myBall.getYVel() * elapsedTime);
+		}
 	}
 	
 	private void handleKeyInput(KeyCode code) {
-		//the additional checks for if the paddle is about to go off the screen
+		//the additional boolean checks for if the paddle is about to go off the screen
 		if(code == KeyCode.RIGHT
 				&& myPaddle.getX() + myPaddle.getWidth() <= SIZE) {
 			myPaddle.setX(myPaddle.getX() + myPaddle.getSpeed());
@@ -64,6 +87,10 @@ public class Breakout extends Application{
 		else if(code == KeyCode.LEFT 
 				&& myPaddle.getX() >= 0) {
 			myPaddle.setX(myPaddle.getX() - myPaddle.getSpeed());
+		}
+		//checks to see if the player has started the game
+		else if(code == KeyCode.UP) {
+			gameStart = true;
 		}
 	}
 	
