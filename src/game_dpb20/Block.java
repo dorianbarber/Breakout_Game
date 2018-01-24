11cb917cc50101aba1d0a79dc1625383915a53e7
Dorian Barber <dpb20@duke.edu>
2018-01-24 01:32:53 -0500

    Block javadoc

--- src/game_dpb20/Block.java
+++ src/game_dpb20/Block.java
@@ -4,6 +4,14 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
+/**
+ * Purpose is to represent blocks in the Breakout game
+ * Assumption that the constructor is used otherwise the 
+ * blocks would not show up in the game
+ * 
+ * @author Dorian
+ *
+ */
 public class Block extends Rectangle{
 	public static final int WIDTH = 50;
 	public static final int HEIGHT = 30;
@@ -21,7 +29,12 @@ public class Block extends Rectangle{
 		new Expand()
 	};
 	
-	
+	/**
+	 * Construct block with WIDTH and HEIGHT and 
+	 * number of hits equal to @param numb as well as
+	 * the color it turns when the block dies --> (assumes it will be the background color of the Breakout scene)
+	 * @param back
+	 */
 	public Block(int numb, Paint back) {
 		super(WIDTH, HEIGHT);
 		hitNumber = numb;
@@ -32,15 +45,26 @@ public class Block extends Rectangle{
 		deadColor = back;
 	}
 	
-	
+	/**
+	 * @return HEIGHT of the block
+	 */
 	public static int getBlockHeight() {
 		return HEIGHT;
 	}
 	
+	/**
+	 * @return WIDTH of the block
+	 */
 	public static int getBlockWidth() {
 		return WIDTH;
 	}
 	
+	/**
+	 * Changes the color of the block depending on how many hits it has left
+	 * The reason why this is not abstracted is because the blocks
+	 * change color as they get hit.
+	 * @return the color of the block
+	 */
 	private Color getColor() {
 		if(hitNumber == 1) {
 			return Color.AQUA;
@@ -57,6 +81,10 @@ public class Block extends Rectangle{
 		return Color.DARKSALMON;
 	}
 	
+	/**
+	 * Checks if the block is broken and
+	 * @return isBroke
+	 */
 	public boolean checkBroke() {
 		if(hitNumber == 0) {
 			isBroke = true;
@@ -64,24 +92,44 @@ public class Block extends Rectangle{
 		return isBroke;
 	}
 	
+	/**
+	 * Sets the block to the presumed background color of the
+	 * Breakout.java scene game
+	 */
 	private void deadBlock() {
 		this.setFill(deadColor);
 		this.setStroke(deadColor);
 	}
 	
+	/**
+	 * used to figure out if the ball can break the block
+	 * @return whether the boolean isPermanent
+	 */
 	public boolean isPerm() {
 		return isPermanent;
 	}
 	
+	/**
+	 * @return true if it contains a powerup and gets broken
+	 * @return false if it is only one or neither
+	 */
 	public boolean isPowed() {
 		return hasPowerup && isBroke;
 	}
 	
+	/**
+	 * @return the Powerup attached to the block if it exists
+	 */
 	public Powerup getPowerup() {
 		return pow;
 	}
 	
-	
+	/**
+	 * Checks which side of the block gets hit and makes the 
+	 * ball bounce in the corresponding way
+	 * Updates hitNumber, the color, and checks whether the block has broken
+	 * @param ball 
+	 */
 	public void ballContact(Bouncer ball) {
 		//checks if the ball has hit the top or bottom of the block
 		if(ball.getCenterX() > this.getX() && ball.getCenterX() < this.getX() + this.getWidth()) {
@@ -102,10 +150,12 @@ public class Block extends Rectangle{
 		}
 	}
 	
-	//uses a random functon to decide what kind of block
-	// 20% chance it will be permanent
-	// 30% chance it will have a powerup
-	// 50% chance it will be normal
+	/**
+	 * Selects the type of block during instantiation (called in the constructor)
+	 * There is a 20% chance it will be permanent
+	 * 			  30% chance it will have a powerup
+	 * 			  50% chance it will be a normal block
+	 */
 	private void typeOfBlock() {
 		int rand = (int)(Math.random()*10);
 		if(rand == 4 || rand == 5) {
